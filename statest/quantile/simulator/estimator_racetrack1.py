f55ede4db2862af5c337afd6a4e38379024fee23
ryu577 <rohitpandey576@gmail.com>
2022-01-07 19:22:53 -0800

    Fixed all warnings

--- statest/quantile/simulator/estimator_racetrack1.py
+++ statest/quantile/simulator/estimator_racetrack1.py
@@ -1,6 +1,15 @@
-from statest.quantile.estimate import *
-from statest.quantile.expon_based_estimators import prcntl, prcntl2, prcntl3, prcntl4, prcntl5
-from statest.quantile.some_distributions import *
+from statest.quantile.estimate import est_1, est_2, est_3,\
+                                      est_4, est_5, est_6,\
+                                      est_7, est_8, est_9
+from statest.quantile.expon_based_estimators import prcntl, prcntl2,\
+                                                    prcntl3, prcntl4
+from statest.quantile.some_distributions import rvs_fn1, rvs_fn2,\
+                                                rvs_fn3, rvs_fn4,\
+                                                rvs_fn5, rvs_fn6,\
+                                                ppf_fn1, ppf_fn2,\
+                                                ppf_fn3, ppf_fn4,\
+                                                ppf_fn5, ppf_fn6,\
+                                                distributions_holder
 from statest.quantile.perf_measurer import PrcntlEstPerfMeasurer
 import numpy as np
 from numpy import genfromtxt
@@ -18,7 +27,7 @@ class RaceTrack():
         self.n = n
         self.rvs_fn = distributions_holder[distr_name][0]
         self.ppf_fn = distributions_holder[distr_name][1]
-        
+
         # The percentiles to compare performance for.
         self.qs = np.arange(0.01, 1.0, 0.03)
         # Enumerate the estimators.
@@ -36,10 +45,10 @@ class RaceTrack():
                       "r_strat8", "r_strat9", "expon_bias_m=2",
                       "expon_mle", "expon_bias_max_m"]
 
-    def main1(self):
+    def race(self):
         prf_results = []
-        #fig1, (ax1, ax3) = plt.subplots(2, 1)
-        #fig2, (ax2, ax4) = plt.subplots(2, 1)
+        # fig1, (ax1, ax3) = plt.subplots(2, 1)
+        # fig2, (ax2, ax4) = plt.subplots(2, 1)
         fig1, ax1 = plt.subplots(1, 1)
         fig2, ax2 = plt.subplots(1, 1)
         fig3, ax3 = plt.subplots(1, 1)
@@ -57,7 +66,8 @@ class RaceTrack():
             prf1.simulate()
             prf_results.append(prf1)
             ax1.plot(self.qs, prf1.u_errs, label="Bias for " + name)
-            ax2.plot(self.qs, prf1.u_stds, label="Standard deviation for " + name)
+            ax2.plot(self.qs, prf1.u_stds,
+                     label="Standard deviation for " + name)
             ax3.plot(self.qs, prf1.u_medians, label="DelMedian for " + name)
             ax4.plot(self.qs, prf1.u_mses, label="MSE for " + name)
 
@@ -84,50 +94,56 @@ class RaceTrack():
         base_path = self.plots_save_dir + self.distr_name + "/" + name
         if not os.path.exists(base_path):
             os.makedirs(base_path)
-        fig1.savefig(self.plots_save_dir + self.distr_name + "/" + name + "/biases.png")
-        fig2.savefig(self.plots_save_dir + self.distr_name + "/" + name + "/st_devs.png")
-        fig3.savefig(self.plots_save_dir + self.distr_name + "/" +
-                    name + "/del_medians.png")
-        fig4.savefig(self.plots_save_dir + self.distr_name + "/" + name + "/mses.png")
-        plt.xlabel("Percentile (q)")
+        fig1.savefig(self.plots_save_dir +
+                     self.distr_name + "/" + name + "/biases.png")
+        fig2.savefig(self.plots_save_dir +
+                     self.distr_name + "/" + name + "/st_devs.png")
+        fig3.savefig(self.plots_save_dir +
+                     self.distr_name + "/" +
+                     name + "/del_medians.png")
+        fig4.savefig(self.plots_save_dir +
+                     self.distr_name + "/" + name + "/mses.png")
         plt.show()
         return prf_results
 
-    def make_plots_from_disk(self):
+    def make_plots_from_disk(self,
+                             names_excl={},
+                             names_incl=None):
+        if names_incl is None:
+            names_incl = set(self.names)
         fig1, ax1 = plt.subplots(1, 1)
         fig2, ax2 = plt.subplots(1, 1)
         fig3, ax3 = plt.subplots(1, 1)
         fig4, ax4 = plt.subplots(1, 1)
         qs = genfromtxt(self.data_save_dir + "/qs.csv", delimiter=",")
         self.res_df = pd.DataFrame()
-        for name in self.names:
-            u_errs = genfromtxt(self.data_save_dir + self.distr_name + "/" +
-                                name + "/u_errs.csv", delimiter=',')
-            u_stds = genfromtxt(self.data_save_dir + self.distr_name + "/" +
-                                name + "/u_stds.csv", delimiter=',')
-            u_medians = genfromtxt(self.data_save_dir + self.distr_name + "/" +
-                                   name + "/u_medians.csv", delimiter=',')
-            u_mses = genfromtxt(self.data_save_dir + self.distr_name + "/" +
-                                name + "/u_mses.csv", delimiter=',')
-            self.res_df[name] = u_errs
-            ax1.plot(qs, u_errs, label="Bias for " + name)
-            ax2.plot(qs, u_stds, label="Standard deviation for " + name)
-            ax3.plot(qs, u_medians, label="DelMedian for " + name)
-            ax4.plot(qs, u_mses, label="MSE for " + name)
+        self.res_df["qs"] = qs
+        for name in names_incl:
+            if name not in names_excl:
+                u_errs = genfromtxt(self.data_save_dir +
+                                    self.distr_name + "/" +
+                                    name + "/u_errs.csv", delimiter=',')
+                u_stds = genfromtxt(self.data_save_dir +
+                                    self.distr_name + "/" +
+                                    name + "/u_stds.csv", delimiter=',')
+                u_medians = genfromtxt(self.data_save_dir +
+                                       self.distr_name + "/" +
+                                       name + "/u_medians.csv", delimiter=',')
+                u_mses = genfromtxt(self.data_save_dir +
+                                    self.distr_name + "/" +
+                                    name + "/u_mses.csv", delimiter=',')
+                self.res_df[name] = u_errs
+                ax1.plot(qs, u_errs, label="Bias for " + name)
+                ax2.plot(qs, u_stds, label="Standard deviation for " + name)
+                ax3.plot(qs, u_medians, label="DelMedian for " + name)
+                ax4.plot(qs, u_mses, label="MSE for " + name)
         make_lines(ax1, ax2, ax3, ax4)
         plt.show()
 
 
 def make_lines(ax1, ax2, ax3, ax4):
-    ax1.axhline(0, color="black")
-    ax1.axvline(0.5, color="black")
-    ax2.axhline(0, color="black")
-    ax2.axvline(0.5, color="black")
-    ax3.axhline(0, color="black")
-    ax3.axvline(0.5, color="black")
-    ax4.axhline(0, color="black")
-    ax4.axvline(0.5, color="black")
-    ax1.legend()
-    ax2.legend()
-    ax3.legend()
-    ax4.legend()
+    for ax in [ax1, ax2, ax3, ax4]:
+        ax.xlabel = "Percentile (q)"
+        ax.axhline(0, color="black")
+        ax.axvline(0.5, color="black")
+        ax.legend()
